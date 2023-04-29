1aba3e012633870838ddea080d9c4d4d7c9a91f9
Apsalus <114671190+Apsalus@users.noreply.github.com>
2023-04-29 14:38:31 +0900

    20230429

--- comm/src/main/java/edu/kh/comm/main/controller/ExceptionController.java
+++ comm/src/main/java/edu/kh/comm/main/controller/ExceptionController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class ExceptionController {
 	
+	
+	
+	
 	@ExceptionHandler(Exception.class)
 	public String exceptionHandler(Exception e, Model model) {
 		e.printStackTrace();
